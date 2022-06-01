8997e8e0ca09fdd675cd1b517916ade7c89aee9e
mohankumar17 <kothalamohankumar17@gmail.com>
2022-06-01 08:24:17 +0530

    Webpack

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Counter } from './Counter'
+
+export const App = () => {
+  return (
+    <div>
+      <h1>React TypeScript Webpack Starter Template</h1>
+      <Counter />
+    </div>
+  )
+}
